77f756187e68dc766007c08ee5b99ec8d59d19a3
Luís Costa <Lyphos94@gmail.com>
2021-08-05 18:24:14 -0300

    Update game over location

--- game.js
+++ game.js
@@ -9,7 +9,7 @@ const gameBoard = document.getElementById("game-board");
 function main(currentTime) {
     if (gameOver) {
     if (confirm('You lost. Press Ok to Restart')) {
-        window.location = '/Snake-game/'
+        window.location.href = 'https://luiscosta94.github.io/Snake-game/'
     }
     return 
 }
